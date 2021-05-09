0abbdeee50dfa8dc9fbb7f16fade513855d0686f
Michael Francisco <m.francisco9@gmail.com>
2021-05-08 20:45:18 -0500

    product model finished

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define columns\
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DateTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+    },
+    stock: {
+      type: DateTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+
+    },
+    category_id: {
+      type: DateTypes.INTEGER,
+      referennces: {
+        model: 'category',
+        key: 'id',
+      }
+
+    }
+
   },
   {
     sequelize,

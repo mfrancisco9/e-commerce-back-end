04d5b5219ac24d6027923f4aaa60f087c36e0101
Michael Francisco <m.francisco9@gmail.com>
2021-05-08 19:44:33 -0500

    category finished

--- models/Category.js
+++ models/Category.js
@@ -8,10 +8,14 @@ Category.init(
   {
     // define columns
     id: {
-
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     category_name: {
-      
+      type: DataTypes.STRING,
+      allowNull: false,
     }
   },
   {
